4617b65162b0e95d8bc8d504907330fe58fa2059
r3mot github <88360543+r3mot@users.noreply.github.com>
2022-03-11 17:45:33 -0800

    Added library class to store catalog of books

---
+++ src/main/java/midterm/Backend/Library.java
@@ -0,0 +1,26 @@
+package midterm.Backend;
+
+public class Library {
+
+    Generator random = new Generator();
+    
+    private Book[] catalog; // array of book objects
+    private int bookCount = 20; // Initial array has 20 randomly generated books
+
+    public Book[] getCatalog(){
+        return catalog;
+    }
+
+    public Book addBook(String t, String s, int p, int y, int r){
+        Book newBook = new Book(t, s, p, y, r);
+        catalog[bookCount] = newBook;
+        bookCount++;
+        return newBook;
+    }
+
+    /* Randomly generates the first 20 books in the catalog */
+    public void initialize(){
+        catalog = new Book[50];
+        catalog = random.getRandomBooks();
+    }
+}

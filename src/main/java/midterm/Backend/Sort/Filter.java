255b919f44a26a32c42873513050eaeac5cbc217
r3mot github <88360543+r3mot@users.noreply.github.com>
2022-03-11 18:45:44 -0800

    added quick sort and filter methods

---
+++ src/main/java/midterm/Backend/Sort/Filter.java
@@ -0,0 +1,24 @@
+package midterm.Backend;
+
+public class Filter extends QuickSort {
+
+    private static int PAGES = 1;
+    private int YEAR = 2;
+    private int RATING = 3;
+    private Book[] toSort;
+
+    public Filter(Book[] toSort){
+        this.toSort = toSort;
+    }
+    public Book[] filterPages(){
+        return sort(toSort, PAGES);
+    }
+
+    public Book[] filterYear(){
+        return sort(toSort, YEAR);
+    }
+
+    public Book[] filterRating() {
+        return sort(toSort, RATING);
+    }
+}

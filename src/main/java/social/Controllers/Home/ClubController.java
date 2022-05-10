ec0ea3f6b077f232d99d836fd6d0e397a2bb34b9
Corey <mendrellac@gmail.com>
2022-05-10 15:28:16 -0700

    Created 'post' object to dynamically create posts to display on the dashboard as well as the user profile

---
+++ src/main/java/social/Controllers/Home/ClubController.java
@@ -0,0 +1,13 @@
+package social.Controllers.Home;
+
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class ClubController {
+
+    @FXML private AnchorPane scrollAnchor;
+
+    
+
+}
---
+++ src/main/java/social/Objects/Post.java
@@ -0,0 +1,91 @@
+package social.Objects;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
+
+public class Post extends Pane {
+    
+    private int PREF_WIDTH = 763;
+    private int PREF_HEIGHT = 250;
+
+    private Label dateLabel;
+    private Label nameLabel;
+    private Circle picture;
+    private TextArea contentTA;
+
+    private String name;
+    private String content;
+    private String imageURL;
+    private String date;
+    private int current_y;
+
+    public Post(String name, String content, String imageURL, String date, int current_y){
+
+        this.name = name;
+        this.content = content;
+        this.imageURL = imageURL;
+        this.date = date;
+        this.current_y = current_y;
+
+        createPost();
+    }
+
+    private void createPost(){
+
+        this.setPrefWidth(PREF_WIDTH);
+        this.setPrefHeight(PREF_HEIGHT);
+        this.setLayoutY(current_y);
+    
+        setImage();
+        setName();
+        setDate();
+        setContent();
+
+        this.getChildren().addAll(dateLabel, nameLabel, picture, contentTA);
+    }
+
+    private void setContent(){
+
+        contentTA = new TextArea();
+        contentTA.setId("post-text-area");
+        contentTA.setPrefWidth(622);
+        contentTA.setPrefHeight(74);
+        contentTA.setLayoutX(23);
+        contentTA.setLayoutY(85);
+        contentTA.setDisable(true);
+        contentTA.setWrapText(true);
+        contentTA.setText(this.content);
+
+    }
+
+    private void setDate(){
+
+        dateLabel = new Label();
+        dateLabel.setId("post-date-label");
+        dateLabel.setPrefWidth(160);
+        dateLabel.setPrefHeight(17);
+        dateLabel.setLayoutX(83);
+        dateLabel.setLayoutY(40);
+        dateLabel.setText(this.date);
+
+    }
+
+    private void setName(){
+
+        nameLabel = new Label();
+        nameLabel.setId("post-name-label");
+        nameLabel.setPrefWidth(126);
+        nameLabel.setPrefHeight(17);
+        nameLabel.setLayoutX(83);
+        nameLabel.setLayoutY(19);
+        nameLabel.setText(this.name);
+    }
+
+    private void setImage(){
+        
+        //TODO: Get user image from SQL database
+
+    }
+}

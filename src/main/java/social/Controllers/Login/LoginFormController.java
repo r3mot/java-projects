814fd2fb2c94d66547aff5d78df107f45694753a
Corey <mendrellac@gmail.com>
2022-05-10 17:24:55 -0700

    Implemented login functionality, fetching credentials from database

--- src/main/java/social/Controllers/Login/LoginFormController.java
+++ src/main/java/social/Controllers/Login/LoginFormController.java
@@ -1,14 +1,17 @@
 package social.Controllers.Login;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import social.App;
+import social.Database.CredentialManager;
 import social.Objects.CurrentUser;
 
 public class LoginFormController {
@@ -17,36 +20,36 @@ public class LoginFormController {
     @FXML private Button loginButton;
     @FXML private PasswordField password;
     @FXML private TextField username;
+    @FXML private Label loginError;
 
-    /**
-     * 
-     * @param event
-     * 
-     * Goes to homescreen on successfull login
-     * @throws IOException
-     */
+    private CredentialManager credentials = new CredentialManager();
+
+
     @FXML
-    void login(ActionEvent event) throws IOException {
+    void login(ActionEvent event) throws IOException, ClassNotFoundException, SQLException {
 
-        boolean login = true;
-        process(login);
+        if(success()){
+            execLogin();
+        }
+
+        showError();
+    }
+
+    private void execLogin() throws IOException{
+
+        CurrentUser.setUsername(username.getText());
+        App.setRoot("Home");
 
     }
 
-    /**
-     * 
-     * @param success
-     * 
-     * Validates user input
-     * @throws IOException
-     */
-    private void process(boolean success) throws IOException{
 
-        if(success)
-        {
-            CurrentUser.setUsername(username.getText());
-            App.setRoot("Home");
-        }
+    private boolean success() throws ClassNotFoundException, SQLException{
+        return credentials.login(username.getText(), password.getText());
+    }
+
+
+    private void showError(){
+        loginError.setText("Invalid Username or Password");
     }
 
 }

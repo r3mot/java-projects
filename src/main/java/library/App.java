34c70d728ca90d0365f5879f26da262a623de894
r3mot <cmendrella1@toromail.csudh.edu>
2022-12-06 01:40:14 -0800

    fix: undecorated stage draggable

--- src/main/java/library/App.java
+++ src/main/java/library/App.java
@@ -1,23 +1,27 @@
 package library;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
-
 import java.io.IOException;
 
 /* Start of main program */
 public class App extends Application {
 
     private static Scene scene;
+    private static double xOffset = 0;
+    private static double yOffset = 0;
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Home"), 1304, 739);
         String css = this.getClass().getResource("/library/Styling/style.css").toExternalForm();
+        scene = new Scene(loadFXML("Home"), 1304, 739);
+        addDragListener(stage);
         scene.getStylesheets().add(css);
         stage.initStyle(StageStyle.UNDECORATED);
         stage.setScene(scene);
@@ -31,6 +35,24 @@ public class App extends Application {
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
+    }
+
+    private static void addDragListener(Stage stage){
+        scene.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX() - xOffset);
+                stage.setY(event.getScreenY() - yOffset);
+            }
+        });
+
     }
 
     public static void main(String[] args) {

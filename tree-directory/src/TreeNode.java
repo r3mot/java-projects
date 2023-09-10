d0e3058daba514e781fc47af2fec5370c4ad8ad7
r3mot <cmendrella1@toromail.csudh.edu>
2023-09-10 10:46:26 -0700

    feat(sample code): add treenode sample code

---
+++ tree-directory/src/TreeNode.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TreeNode {
+    private String value;
+    private List<TreeNode> children;
+
+    public TreeNode(String value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    // Add a child node
+    public void addChild(TreeNode child) {
+        children.add(child);
+    }
+
+    // Print the tree structure
+    public void print() {
+        print("", true);
+    }
+
+    private void print(String prefix, boolean isTail) {
+        System.out.println(prefix + (isTail ? "??? " : "??? ") + value);
+        for (int i = 0; i < children.size() - 1; i++) {
+            children.get(i).print(prefix + (isTail ? "    " : "?   "), false);
+        }
+        if (children.size() > 0) {
+            children.get(children.size() - 1).print(prefix + (isTail ? "    " : "?   "), true);
+        }
+    }
+}

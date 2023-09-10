87437aff2ede13295b0c3ca149849a76125e8cbc
r3mot <cmendrella1@toromail.csudh.edu>
2023-09-10 14:35:30 -0700

    feat(main): allow execution of nqueen

--- n-queen/src/App.java
+++ n-queen/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        NQueen nQueen = new NQueen(8);
+        nQueen.solve();
     }
 }

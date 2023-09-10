3b2853cf26e3a1aade713c22e3d1c9dd81e7e3b0
r3mot <cmendrella1@toromail.csudh.edu>
2023-09-10 14:43:51 -0700

    feat(main): allow cmd input and standard input

--- n-queen/src/App.java
+++ n-queen/src/App.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        NQueen nQueen = new NQueen(8);
+
+        int n = (args.length > 0) ? Integer.parseInt(args[0]) : getN();
+
+        NQueen nQueen = new NQueen(n);
         nQueen.solve();
+    }
+
+    public static int getN() {
+
+        int n = 4;
+        Scanner userIn = new Scanner(System.in);
+        System.out.println("Enter a number for (n): ");
+        if (userIn.hasNextLine()) {
+            n = Integer.parseInt(userIn.nextLine());
+        }
+
+        userIn.close();
+
+        return n;
     }
 }

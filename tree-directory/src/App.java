a558d5208f5c596285a0d9917910dcacb10247c9
r3mot <cmendrella1@toromail.csudh.edu>
2023-09-10 12:12:36 -0700

    feat(cmd): allow path to be passed as command line argument

--- tree-directory/src/App.java
+++ tree-directory/src/App.java
@@ -1,12 +1,29 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CLI cli = new CLI();
+
+        if (args.length > 0) {
+            parseDirectory(args[0]);
+            return;
+        } else {
+            Scanner userIn = new Scanner(System.in);
+            System.out.println("Enter a path to parse: ");
+            if (userIn.hasNextLine()) {
+                parseDirectory(userIn.nextLine());
+                userIn.close();
+                return;
+            }
+            userIn.close();
+        }
+    }
+
+    public static void parseDirectory(String path) {
 
         try {
 
-            File directory = new File(cli.getPath());
+            File directory = new File(path);
             TreeNode root = new TreeNode(directory.getName());
 
             if (!root.buildTree(directory)) {
@@ -19,8 +36,6 @@ public class App {
         } catch (Exception e) {
             System.err.println("Path does not exist");
         }
-
-        cli.close();
     }
 
 }

e05b679001cf56a44b89c2991a6f7ee529e6ea73
r3mot <cmendrella1@toromail.csudh.edu>
2023-09-23 07:33:19 -0700

    refactor(app): demo  of new features

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
 import registrar.Registrar;
+import registrar.objects.Student;
+import registrar.objects.Course;
 
 public class App {
     public static void main(String[] args) throws Exception {
@@ -6,7 +8,22 @@ public class App {
         int numCourses = 10;
         Registrar registrar = new Registrar(numCourses);
 
-        registrar.dumpStudents();
-        registrar.dumpCourses();
+        registrar.printAllStudents(); // list view
+        registrar.printAllCourses(); // list view
+        registrar.printCoursesAsTree(); // tree view
+
+        String studentId = "";
+        Student student = registrar.getStudentById(studentId);
+        student.printCourses();
+
+        String courseCode = "";
+        Course course = registrar.getCourseByCode(courseCode);
+        course.printRoster();
+
+        // adding
+        registrar.enrollStudent(studentId, courseCode);
+
+        // dropping
+        registrar.removeStudent(studentId, courseCode);
     }
 }

b938cc376cac5e1a80fbfa524ad8de6f62ce5b37
r3mot <cmendrella1@toromail.csudh.edu>
2023-12-02 15:03:55 -0800

    feat: render view from main function

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialDarkerIJTheme;
+import controller.Controller;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    FlatMaterialDarkerIJTheme.setup(); // theme for the app
+    Controller controller = new Controller();
+    controller.renderView();
   }
 }

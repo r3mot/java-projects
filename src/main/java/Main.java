7ba74057100f021abb62ee9e3111070838a9f227
r3mot <cmendrella1@toromail.csudh.edu>
2023-12-04 15:02:23 -0800

    refactor: change default ui arc and focus width

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
-import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatMaterialDarkerIJTheme;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkIJTheme;
 import controller.Controller;
+import javax.swing.UIManager;
 
 /**
  * A simple application that connects a MySQL database to a Java application.
@@ -8,7 +9,9 @@ public class Main {
 
   public static void main(String[] args) {
     // theme for the app (FlatLaf - dependency in pom.xml)
-    FlatMaterialDarkerIJTheme.setup();
+    FlatAtomOneDarkIJTheme.setup();
+    UIManager.put("Component.focusWidth", 0);
+    UIManager.put("TextComponent.arc", 5);
 
     // main controller for the application
     Controller controller = new Controller();
